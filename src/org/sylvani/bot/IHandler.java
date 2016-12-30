33dcdb6640545d85dc07d89eb91d421a298fb09f
harald.kuhn@gmail.com <harald.kuhn@gmail.com>
2016-12-30 18:48:37 +0100

    cleanup and comments

--- src/org/sylvani/bot/IHandler.java
+++ src/org/sylvani/bot/IHandler.java
@@ -2,8 +2,14 @@ package org.sylvani.bot;
 
 import org.sylvani.bot.connector.ms.model.Activity;
 
+/**
+ * Generic handler for activities. Defines a common api for interceptors and
+ * dialogs
+ * 
+ * @author Harald Kuhn
+ */
 public interface IHandler {
 
-	void handle(ISession context, Activity activity);
+	void handle(ISession session, Activity activity);
 
 }
--- src/org/sylvani/bot/ISession.java
+++ src/org/sylvani/bot/ISession.java
@@ -2,9 +2,19 @@ package org.sylvani.bot;
 
 import org.sylvani.bot.connector.ms.model.Activity;
 
+/**
+ * chat session
+ * 
+ * @author Harald Kuhn
+ */
 public interface ISession extends IContext {
-
+	/**
+	 * send the given activity
+	 * 
+	 * @param activity
+	 */
 	public void sendAsync(Activity activity);
 
 	public IBotContext getBotContext();
+
 }
