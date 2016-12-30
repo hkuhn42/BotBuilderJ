f4567b62db63ae2a89b2f764868d1b833c2ed4d7
Harald Kuhn <harald.kuhn@gmail.com>
2016-12-30 18:47:46 +0100

    Basic waterfall dialog

---
+++ src/org/sylvani/bot/dialogs/WaterfallDialog.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package org.sylvani.bot.dialogs;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+import org.sylvani.bot.ISession;
+import org.sylvani.bot.connector.ms.model.Activity;
+
+/**
+ * Simple waterfall dialog subdialogs are processes in sequence
+ * 
+ * @author Harald Kuhn
+ */
+public class WaterfallDialog implements IDialog {
+
+	private static final String	DIALOG_STATE_PREFIX	= "WaterfallDialog";
+
+	private List<IDialog>		dialogs;
+	private String				instanceStateKey;
+
+	public WaterfallDialog() {
+		this.dialogs = new ArrayList<>();
+		this.instanceStateKey = DIALOG_STATE_PREFIX + "." + UUID.randomUUID().toString();
+	}
+
+	public WaterfallDialog(IDialog[] dialogs) {
+		this();
+		this.dialogs.addAll(Arrays.asList(dialogs));
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.sylvani.bot.IHandler#handle(org.sylvani.bot.ISession, org.sylvani.bot.connector.ms.model.Activity)
+	 */
+	@Override
+	public void handle(ISession session, Activity activity) {
+		Integer instanceState = (Integer) session.getAttribute(instanceStateKey);
+		if (instanceState == null) {
+			instanceState = new Integer(0);
+		}
+		else if (instanceState >= dialogs.size()) {
+			instanceState = new Integer(0);
+		}
+		IDialog activeDialog = dialogs.get(instanceState.intValue());
+
+		instanceState++;
+		session.setAttribute(instanceStateKey, instanceState);
+
+		activeDialog.handle(session, activity);
+	}
+
+}
