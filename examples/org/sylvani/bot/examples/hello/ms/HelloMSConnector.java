60db88fa1b1b8812bd13bd7db55c748579e7d0ae
harald.kuhn@gmail.com <harald.kuhn@gmail.com>
2017-05-01 21:25:47 +0200

    Hello World example with  the ms connector

--- examples/org/sylvani/bot/examples/hello/ms/HelloMSConnector.java
+++ examples/org/sylvani/bot/examples/hello/ms/HelloMSConnector.java
@@ -1,31 +1,16 @@
 package org.sylvani.bot.examples.hello.ms;
 
-import org.sylvani.bot.IActivity;
-import org.sylvani.bot.ISession;
+import org.apache.log4j.BasicConfigurator;
 import org.sylvani.bot.connector.ms.MSConnector;
-import org.sylvani.bot.dialogs.IDialog;
-import org.sylvani.bot.universal.UniversalBot;
-import org.sylvani.bot.util.Prompt;;
+import org.sylvani.bot.examples.hello.HelloBot;;
 
 /**
  * This example uses the msConnector for a hello world
  */
-public class HelloBot extends UniversalBot {
-
-	public HelloBot() {
-		super(new MSConnector());
-	}
+public class HelloMSConnector {
 
 	public static void main(String[] args) {
-		HelloBot bot = new HelloBot();
-
-		bot.add(".*", new IDialog() {
-
-			@Override
-			public void handle(ISession session, IActivity activity) {
-				session.send(Prompt.answer("Hello World", activity));
-			}
-		});
-
+		BasicConfigurator.configure();
+		new HelloBot(new MSConnector());
 	}
 }
