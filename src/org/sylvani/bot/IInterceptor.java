a74e337863eeb58e428c040c2280ec0307379fc2
harald.kuhn@gmail.com <harald.kuhn@gmail.com>
2017-05-05 22:02:09 +0200

    Added init method for chaining interceptors

--- src/org/sylvani/bot/IInterceptor.java
+++ src/org/sylvani/bot/IInterceptor.java
@@ -6,8 +6,12 @@ package org.sylvani.bot;
 /**
  * An interceptor / filter for incoming Activities
  * 
+ * Interceptors must throw a NullPointerException if init is not called once before handle
+ * 
  * @author hkuhn
  */
 public interface IInterceptor extends IHandler {
+
+	public void init(IHandler nextInChain);
 
 }
